0cf2e2b99645666d314c9766e870a579fd61c885
zeldahyrule-bellaoni <hyrulezelda504@gmail.com>
2026-01-24 10:51:54 +0530

    Update run-map.js

--- run-map.js
+++ run-map.js
@@ -1,4 +1,7 @@
-module.exports = { /*
+module.exports = {
+
+  //REMOVE OR PLACE COMMENT BELOW
+  /*
   oddDayRuns: {
     1: [
       { tierId: 10, startPage: 1,   endPage: 7 },
@@ -109,62 +112,67 @@ module.exports = { /*
       { tierId: 7, startPage: 111, endPage: 121 },
       { tierId: 7, startPage: 122, endPage: 127 }
     ]
-  } */
-
-  //---------------------------------------------------------------------------------------------------
-  evenDayRuns: {
+  } //REMOVE OR PLACE COMMENT BELOW 
+  */
+  
+  //===============================================================
+  // =========== LEVELS 20 TO 34 ==================================
+  //===============================================================
+  // COMMENT BELOW
+  
+  evenDayRuns: { // change even or add according to convenience
     1: [
-      { tierId: 10, startPage: 1,   endPage: 7 },
-      { tierId: 10, startPage: 8,   endPage: 14 },
-      { tierId: 10, startPage: 15,  endPage: 21 },
-      { tierId: 10, startPage: 22,  endPage: 28 },
-      { tierId: 10, startPage: 29,  endPage: 35 },
-      { tierId: 10, startPage: 36,  endPage: 42 }
+      { tierId: 6, startPage: 1,   endPage: 12 },
+      { tierId: 6, startPage: 13,  endPage: 24 },
+      { tierId: 6, startPage: 25,  endPage: 36 },
+      { tierId: 6, startPage: 37,  endPage: 48 },
+      { tierId: 6, startPage: 49,  endPage: 60 },
+      { tierId: 6, startPage: 61,  endPage: 72 }
     ],
-
+    
     2: [
-      { tierId: 10, startPage: 43, endPage: 49 },
-      { tierId: 10, startPage: 50, endPage: 56 },
-      { tierId: 10, startPage: 57, endPage: 63 },
-      { tierId: 10, startPage: 64, endPage: 70 },
-      { tierId: 10, startPage: 71, endPage: 77 },
-      { tierId: 10, startPage: 78, endPage: 84 }
+      { tierId: 6, startPage: 73,  endPage: 84 },
+      { tierId: 6, startPage: 85,  endPage: 96 },
+      { tierId: 6, startPage: 97,  endPage: 108 },
+      { tierId: 6, startPage: 109, endPage: 120 },
+      { tierId: 6, startPage: 121, endPage: 132 },
+      { tierId: 6, startPage: 133, endPage: 145 }
     ],
-
+    
     3: [
-      { tierId: 10, startPage: 85, endPage: 91 },
-      { tierId: 10, startPage: 92, endPage: 98 },
-      { tierId: 10, startPage: 99, endPage: 105 },
-      { tierId: 10, startPage: 106, endPage: 112 },
-      { tierId: 10, startPage: 113, endPage: 119 },
-      { tierId: 10, startPage: 120, endPage: 126 }
+      { tierId: 5, startPage: 1,   endPage: 16 },
+      { tierId: 5, startPage: 17,  endPage: 32 },
+      { tierId: 5, startPage: 33,  endPage: 48 },
+      { tierId: 5, startPage: 49,  endPage: 64 },
+      { tierId: 5, startPage: 65,  endPage: 80 },
+      { tierId: 5, startPage: 81,  endPage: 96 }
     ],
-
+    
     4: [
-      { tierId: 10, startPage: 127, endPage: 133 },
-      { tierId: 10, startPage: 134, endPage: 140 },
-      { tierId: 10, startPage: 141, endPage: 147 },
-      { tierId: 10, startPage: 148, endPage: 154 },
-      { tierId: 10, startPage: 155, endPage: 161 },
-      { tierId: 10, startPage: 162, endPage: 168 }
+      { tierId: 5, startPage: 97,  endPage: 112 },
+      { tierId: 5, startPage: 113, endPage: 128 },
+      { tierId: 5, startPage: 129, endPage: 144 },
+      { tierId: 5, startPage: 145, endPage: 160 },
+      { tierId: 5, startPage: 161, endPage: 176 },
+      { tierId: 5, startPage: 177, endPage: 194 }
     ],
-
+    
     5: [
-      { tierId: 10, startPage: 169, endPage: 175 },
-      { tierId: 10, startPage: 176, endPage: 182 },
-      { tierId: 10, startPage: 183, endPage: 189 },
-      { tierId: 10, startPage: 190, endPage: 196 },
-      { tierId: 10, startPage: 197, endPage: 203 },
-      { tierId: 10, startPage: 204, endPage: 210 }
+      { tierId: 4, startPage: 1,   endPage: 16 },
+      { tierId: 4, startPage: 17,  endPage: 32 },
+      { tierId: 4, startPage: 33,  endPage: 48 },
+      { tierId: 4, startPage: 49,  endPage: 64 },
+      { tierId: 4, startPage: 65,  endPage: 80 },
+      { tierId: 4, startPage: 81,  endPage: 96 }
     ],
-
+    
     6: [
-      { tierId: 10, startPage: 211, endPage: 217 },
-      { tierId: 10, startPage: 218, endPage: 224 },
-      { tierId: 10, startPage: 225, endPage: 231 },
-      { tierId: 10, startPage: 232, endPage: 238 },
-      { tierId: 10, startPage: 239, endPage: 245 },
-      { tierId: 10, startPage: 246, endPage: 252 }
+      { tierId: 4, startPage: 97,  endPage: 112 },
+      { tierId: 4, startPage: 113, endPage: 128 },
+      { tierId: 4, startPage: 129, endPage: 144 },
+      { tierId: 4, startPage: 145, endPage: 160 },
+      { tierId: 4, startPage: 161, endPage: 176 },
+      { tierId: 4, startPage: 177, endPage: 197 }
     ]
-  }
+  } // comment here
 };
